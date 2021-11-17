4d386689ff36ae39e875d003031c91247a08141d
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-11-16 22:48:37 -0800

    added api fetch variables and functions

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,32 @@ var searchBtnText = $("#text-box")
 
 searchBtnEl.on("click", searchBtnRun);
 $("search-history-list").on("click", ".search-history-btn", historyBtnRun)
+
+// retrieving information from the API
+function searchBtnRun(coordsSource) {
+    var city = searchBtnText.val();
+    var APIKey = "eef440075f231dabd98329edc16d0dae";
+
+    console.log(city);
+    var coordsSource = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+
+    // fetching locational information
+    fetch(coordsSource).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        var latitude = data.coord.latitude
+        console.log(latitude);
+
+        var longitude = data.coord.longitude
+        console.log(longitude);
+
+        var finalAPI = "https://api.openweathermap.org/data/2.5/onecall?lat="+ latitude + "&lon=" + longitude + "&units=imperial&appid=" + APIKey
+
+        fetch(finalAPI).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+            
+        })
+    })
+}
